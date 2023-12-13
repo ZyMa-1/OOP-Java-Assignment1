715f25552cbfa7caa7cf76fa835eb05f07f33ebb
ZyMa-1 <misha01052004@gmail.com>
2023-12-13 12:49:58 +0600

    i have no life

--- src/LibraryReader/Main.java
+++ src/LibraryReader/Main.java
@@ -4,20 +4,22 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        LibraryReader reader = new LibraryReader("John Doe", "12345", "Computer Science", "555-1234", LocalDate.of(2004, 5, 1));
+        LibraryReader reader1 = new LibraryReader("John Doe", "12345", "Computer Science", "555-1234", LocalDate.of(2004, 5, 1));
+        LibraryReader reader2 = new LibraryReader("Mark", "1234", "Software Engineering", "444-1234", LocalDate.of(2004, 5, 1));
+        LibraryReader[] readers = {reader1, reader2};
 
         // Creating some books
         Book book1 = new Book("The Catcher in the Rye", "J.D. Salinger");
         Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
 
         // Testing takeBook methods
-        reader.takeBook(3);
-        reader.takeBook("Introduction to Java", "Data Structures", "Algorithms");
-        reader.takeBook(book1, book2);
+        readers[0].takeBook(3);
+        readers[0].takeBook("Introduction to Java", "Data Structures", "Algorithms");
+        readers[0].takeBook(book1, book2);
 
         // Testing returnBook methods
-        reader.returnBooks(2);
-        reader.returnBooks("Introduction to Java", "Data Structures");
-        reader.returnBooks(book1, book2);
+        readers[1].returnBooks(2);
+        readers[1].returnBooks("Introduction to Java", "Data Structures");
+        readers[1].returnBooks(book1, book2);
     }
 }
